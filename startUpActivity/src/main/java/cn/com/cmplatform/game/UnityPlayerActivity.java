74a470be08f32b30824228bc801c9c0446c9453e
zhangneng <zhangneng.cixi@gmail.com>
2019-04-08 11:05:08 +0800

    删除Androud Studio 3.3.0
    删除Androud Studio 3.3.0工程

--- startUpActivity/src/main/java/cn/com/cmplatform/game/UnityPlayerActivity.java
+++
@@ -1,8 +0,0 @@
-package cn.com.cmplatform.game;
-
-import com.unity3d.player.*;
-
-/**
- * @deprecated Use UnityPlayerNativeActivity instead.
- */
-public class UnityPlayerActivity extends UnityPlayerNativeActivity { }
--- startUpActivity/src/main/java/cn/com/cmplatform/share/WXEntryActivity.java
+++
@@ -1,8 +0,0 @@
-
-package cn.com.cmplatform.share;
-
-import com.umeng.socialize.view.WXCallbackActivity;
-
-public class WXEntryActivity extends WXCallbackActivity {
-
-}
